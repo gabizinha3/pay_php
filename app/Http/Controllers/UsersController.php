215b6d2701367d7cf9b63263620ac83c4e31f2ab
gabriela <gabrielaferranti@hotmail.com>
2021-02-07 02:41:08 -0300

    feat: add load user to controller for route

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Presentation\Controllers\Users\LoadUsersController;
+use App\Models\Users;
+use App\Domain\Usecases\Users\LoadUsersUsecase;
+
+class UsersController extends Controller
+{
+    private $request;
+    private $users;
+
+    public function __construct(Request $request, Users $users)
+    {
+        $this->request = $request;
+        $this->model = $users;
+    }
+
+    private function makeRepository()
+    {
+        return $this->model;
+    }
+
+    private function makeUseCase()
+    {
+        $loadUser = new LoadUsersUsecase($this->makeRepository());
+        return (object) [
+            'loadUser' => $loadUser
+        ];
+    }
+
+    public function makeLoadUsersController()
+    {
+        $usecases = $this->makeUseCase();
+        $controller = new LoadUsersController($usecases->loadUser);
+        return $controller->handle($this->request);
+    }
+}
