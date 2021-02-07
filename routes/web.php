d5d08643720a1f1e65f31815464b518d7418c9eb
gabriela <gabrielaferranti@hotmail.com>
2021-02-07 02:41:31 -0300

    feat: add get users route

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::create('/users', 'UserController@index');
-Route::create('/users', 'UserController@get');
+// Route::create('/users', 'UsersController@index');
+Route::get('/users', 'UsersController@makeLoadUsersController');
