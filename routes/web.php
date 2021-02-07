83701470e0a07c6e78212378100ff0d6db059597
gabriela <gabrielaferranti@hotmail.com>
2021-02-07 00:21:27 -0300

    refactor: remove routes from console, web and channels

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::create('/users', 'UserController@index');
+Route::create('/users', 'UserController@get');
